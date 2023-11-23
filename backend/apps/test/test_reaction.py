dcc1a68e597ee732d84b8c3f3e73b1c8d8274de9
ken999999999 <keLearning2021@outlook.com>
2023-11-23 20:25:53 +0000

    remove all test case

--- backend/apps/test/test_reaction.py
+++
@@ -1,7 +0,0 @@
-from apps.test.client import facebook_clone_test_client
-
-
-def test_get_reaction():
-    response = facebook_clone_test_client.get(
-        "/reactions?post_id=123", headers={"Authorization": "Bearer 123456"})
-    assert response.status_code == 200
