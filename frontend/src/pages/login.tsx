5c69dd5f73d583af495ea3482ddde4523cf25db6
ken999999999 <keLearning2021@outlook.com>
2023-12-01 03:46:15 +0000

    remove useless theme provider

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,16 +1,7 @@
 // pages/login.tsx
 import React, { useState } from "react"
-import {
-  Container,
-  Box,
-  Typography,
-  TextField,
-  Button,
-  CssBaseline,
-} from "@mui/material"
-import { createTheme, ThemeProvider } from "@mui/material/styles"
+import { Box, Typography, TextField, Button } from "@mui/material"
 import useAuth from "@/hooks/useAuth"
-const theme = createTheme()
 import { useRouter } from "next/router"
 import Backdrop from "@/components/Backdrop"
 import Link from "next/link"
@@ -25,7 +16,7 @@ const LoginPage: React.FC = () => {
   const handleSignIn = async () => {
     setLoading(true)
     try {
-      const res = await signIn(email, password)
+      await signIn(email, password)
       router.replace("/")
     } catch (err) {
       console.log(err)
@@ -37,72 +28,62 @@ const LoginPage: React.FC = () => {
 
   return (
     <Backdrop open={!!loading}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Container component="main" maxWidth="xs">
-          <Box
-            flexDirection={"row"}
-            sx={{
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Typography component="h1" variant="h5">
-              Facebook
-            </Typography>
-            <Typography sx={{ mb: 2 }}>
-              Connect with friends and the world around you on Facebook.
-            </Typography>
+      <Box
+        flexDirection={"row"}
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Facebook
+        </Typography>
+        <Typography sx={{ mb: 2 }}>
+          Connect with friends and the world around you on Facebook.
+        </Typography>
 
-            <Box
-              component="form"
-              onSubmit={handleSignIn}
-              noValidate
-              sx={{ mt: 1 }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email address or phone number"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Button
-                onClick={handleSignIn}
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3 }}
-              >
-                Log in
-              </Button>
-              <Link href="/sign-up">
-                <Button fullWidth sx={{ mt: 3, mb: 2 }} variant="outlined">
-                  Sign Up
-                </Button>
-              </Link>
-            </Box>
-          </Box>
-        </Container>
-      </ThemeProvider>
+        <Box component="form" onSubmit={handleSignIn} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email address or phone number"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            onClick={handleSignIn}
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3 }}
+          >
+            Log in
+          </Button>
+          <Link href="/sign-up">
+            <Button fullWidth sx={{ mt: 3, mb: 2 }} variant="outlined">
+              Sign Up
+            </Button>
+          </Link>
+        </Box>
+      </Box>
     </Backdrop>
   )
 }
