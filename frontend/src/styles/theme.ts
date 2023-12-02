ac623a17961fe53fb7f5405bf027fd33f73d6dc7
ken999999999 <keLearning2021@outlook.com>
2023-12-02 04:44:54 +0000

    fixed css problem

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles"
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#f1f2f5",
+    },
+  },
+})
+
+export default theme
