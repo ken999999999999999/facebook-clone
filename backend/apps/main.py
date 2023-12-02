2615dc685d786a6e36a9ab3f5843a6736c8086d1
ken999999999 <keLearning2021@outlook.com>
2023-12-01 21:36:29 -0500

    remove allow credient

--- backend/apps/main.py
+++ backend/apps/main.py
@@ -40,7 +40,6 @@ app.include_router(reaction.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allows specific origins (use ["*"] for all origins)
-    allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
