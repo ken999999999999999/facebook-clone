7f0bbd4c0ca7c4f294d9fa0f89b80e17166d569a
ken999999999 <keLearning2021@outlook.com>
2023-12-02 04:03:57 +0000

    fixed deploy problem

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,7 @@
  */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
