b94cda1c7be9e686b146acb41cd072624714c3fc
ken999999999 <keLearning2021@outlook.com>
2023-12-02 22:59:20 +0000

    update tsconfig.json

--- frontend/src/components/UserListItemSkeleton.tsx
+++ frontend/src/components/UserListItemSkeleton.tsx
@@ -2,68 +2,28 @@ import { Box, Skeleton } from "@mui/material"
 
 interface IUserListItemSkeleton {
   showCircle?: boolean
+  total?: number
 }
 
 const UserListItemSkeleton = ({
+  total = 5,
   showCircle = true,
 }: IUserListItemSkeleton): JSX.Element => {
   return (
     <>
-      <Box display="flex" gap={1} mb={1}>
-        {showCircle && (
-          <Skeleton
-            variant="circular"
-            animation="wave"
-            width={56}
-            height={40}
-          />
-        )}
-        <Skeleton animation="wave" width="100%" />
-      </Box>
-      <Box display="flex" gap={1} mb={1}>
-        {showCircle && (
-          <Skeleton
-            variant="circular"
-            animation="wave"
-            width={56}
-            height={40}
-          />
-        )}
-        <Skeleton animation="wave" width="100%" />
-      </Box>
-      <Box display="flex" gap={1} mb={1}>
-        {showCircle && (
-          <Skeleton
-            variant="circular"
-            animation="wave"
-            width={56}
-            height={40}
-          />
-        )}
-        <Skeleton animation="wave" width="100%" />
-      </Box>
-      <Box display="flex" gap={1} mb={1}>
-        {showCircle && (
-          <Skeleton
-            variant="circular"
-            animation="wave"
-            width={56}
-            height={40}
-          />
-        )}
-        <Skeleton animation="wave" width="100%" />
-      </Box>
-      <Box display="flex" gap={1} mb={1}>
-        {showCircle && (
-          <Skeleton
-            variant="circular"
-            animation="wave"
-            width={56}
-            height={40}
-          />
-        )}
-        <Skeleton animation="wave" width="100%" />
-      </Box>
+      {[...Array(10).keys()].map((item) => (
+        <Box display="flex" gap={1} mb={1} key={item}>
+          {showCircle && (
+            <Skeleton
+              variant="circular"
+              animation="wave"
+              width={56}
+              height={40}
+            />
+          )}
+          <Skeleton animation="wave" width="100%" />
+        </Box>
+      ))}
     </>
   )
 }
