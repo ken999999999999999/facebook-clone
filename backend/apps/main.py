6ac145f001fee6062c74177f66e89970280b6013
ken999999999 <keLearning2021@outlook.com>
2023-12-02 22:09:07 -0500

    update test endpoint

--- backend/apps/main.py
+++ backend/apps/main.py
@@ -10,7 +10,7 @@ from apps.config import settings
 from apps.routers import chat, chatroom, comment, posts, reaction, relationship, users
 import firebase_admin
 from firebase_admin import credentials
-from apps.webSocket.chat.endpoint import chat_endpoint
+from apps.webSocket.chat.endpoint import chat_endpoint, chat_test_endpoint
 from apps.webSocket.chat.validator import connect_chat_validator
 
 
@@ -50,13 +50,7 @@ app.add_middleware(
 app.add_api_websocket_route(
     "/chatroom/{chatroom_id}", chat_endpoint, dependencies=[Depends(connect_chat_validator)])
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: webSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        token = websocket.cookies.get("token")
-        await websocket.send_text(f"Message of {token} was: {data}")
+app.add_api_websocket_route("/test/chatroom/", chat_test_endpoint)
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
