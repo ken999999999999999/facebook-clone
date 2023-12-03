383546a0419a1a6aa0803707987c811115ccdefe
kalong-leung <leonellus0123@gmail.com>
2023-12-03 00:25:14 -0500

    image styling

--- backend/apps/routers/posts.py
+++ backend/apps/routers/posts.py
@@ -67,12 +67,12 @@ async def get_posts_query(db_context: db_context, current_user: current_user, pa
     return [PostOriginalDto(**record) for record in query]
 
 
-@router.get("/${id}/image")
+@router.get("/{id}/image")
 async def get_post_image_query(id: str, db_context:  db_context) -> str:
     post = await db_context.posts.find_one({"_id": ObjectId(id)})
     if (post) is None:
         raise HTTPException(status_code=404, detail=f"Post {id} not found")
-    return Post(post).image
+    return post['image']
 
 
 @router.post("/", dependencies=[Depends(create_post_validator)])
--- frontend/src/components/FeedCard.tsx
+++ frontend/src/components/FeedCard.tsx
@@ -158,6 +158,19 @@ const FeedCard: React.FC<FeedCardProps> = ({ post }) => {
           {post.description}
         </Typography>
 
+        {post.has_image && image ? (
+          <img
+            src={image}
+            loading="lazy"
+            style={{
+              objectFit: "cover",
+              width: "calc(100% + 32px)",
+              maxWidth: "calc(100% + 32px)",
+              transform: "translateX(-16px)",
+            }}
+          />
+        ) : null}
+
         <img
           style={{ cursor: "pointer" }}
           onClick={openReactionModal}
@@ -166,16 +179,6 @@ const FeedCard: React.FC<FeedCardProps> = ({ post }) => {
           src="data:image/svg+xml,%3Csvg fill='none' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath d='M16.0001 7.9996c0 4.418-3.5815 7.9996-7.9995 7.9996S.001 12.4176.001 7.9996 3.5825 0 8.0006 0C12.4186 0 16 3.5815 16 7.9996Z' fill='url(%23paint0_linear_15251_63610)'/%3E%3Cpath d='M16.0001 7.9996c0 4.418-3.5815 7.9996-7.9995 7.9996S.001 12.4176.001 7.9996 3.5825 0 8.0006 0C12.4186 0 16 3.5815 16 7.9996Z' fill='url(%23paint1_radial_15251_63610)'/%3E%3Cpath d='M16.0001 7.9996c0 4.418-3.5815 7.9996-7.9995 7.9996S.001 12.4176.001 7.9996 3.5825 0 8.0006 0C12.4186 0 16 3.5815 16 7.9996Z' fill='url(%23paint2_radial_15251_63610)' fill-opacity='.5'/%3E%3Cpath d='M7.3014 3.8662a.6974.6974 0 0 1 .6974-.6977c.6742 0 1.2207.5465 1.2207 1.2206v1.7464a.101.101 0 0 0 .101.101h1.7953c.992 0 1.7232.9273 1.4917 1.892l-.4572 1.9047a2.301 2.301 0 0 1-2.2374 1.764H6.9185a.5752.5752 0 0 1-.5752-.5752V7.7384c0-.4168.097-.8278.2834-1.2005l.2856-.5712a3.6878 3.6878 0 0 0 .3893-1.6509l-.0002-.4496ZM4.367 7a.767.767 0 0 0-.7669.767v3.2598a.767.767 0 0 0 .767.767h.767a.3835.3835 0 0 0 .3835-.3835V7.3835A.3835.3835 0 0 0 5.134 7h-.767Z' fill='%23fff'/%3E%3Cdefs%3E%3CradialGradient id='paint1_radial_15251_63610' cx='0' cy='0' r='1' gradientUnits='userSpaceOnUse' gradientTransform='rotate(90 .0005 8) scale(7.99958)'%3E%3Cstop offset='.5618' stop-color='%230866FF' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%230866FF' stop-opacity='.1'/%3E%3C/radialGradient%3E%3CradialGradient id='paint2_radial_15251_63610' cx='0' cy='0' r='1' gradientUnits='userSpaceOnUse' gradientTransform='rotate(45 -4.5257 10.9237) scale(10.1818)'%3E%3Cstop offset='.3143' stop-color='%2302ADFC'/%3E%3Cstop offset='1' stop-color='%2302ADFC' stop-opacity='0'/%3E%3C/radialGradient%3E%3ClinearGradient id='paint0_linear_15251_63610' x1='2.3989' y1='2.3999' x2='13.5983' y2='13.5993' gradientUnits='userSpaceOnUse'%3E%3Cstop stop-color='%2302ADFC'/%3E%3Cstop offset='.5' stop-color='%230866FF'/%3E%3Cstop offset='1' stop-color='%232B7EFF'/%3E%3C/linearGradient%3E%3C/defs%3E%3C/svg%3E"
           width="18"
         />
-
-        <Container
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          {post.has_image && image ? <img src={image} loading="lazy" /> : null}
-        </Container>
       </Box>
       <CommentModal
         post={post}
